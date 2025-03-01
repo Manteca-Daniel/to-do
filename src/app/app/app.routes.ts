b040564dc0e29735c5c76fa326e8741a31d4432c
Manteca-Daniel <danielmanteca.dmg1@gmail.com>
2025-03-01 15:55:09 +0100

    Sobre nosotros añadido

--- src/app/app/app.routes.ts
+++ src/app/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { TodoListComponent } from '../todo/todo-list/todo-list.component';
 import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component';
 import { ContactFormComponent } from '../contact-form/contact-form.component';
+import { HomeComponent } from '../home/home.component';
 
 export const routes: Routes = [
   { path: '', component: TodoListComponent },
   { path: 'privacy', component: PrivacyPolicyComponent },
-  { path: 'contact', component: ContactFormComponent }
+  { path: 'contact', component: ContactFormComponent },
+  { path: 'home', component: HomeComponent }
 ];
